77cacfffabcf11ae84c54d37c1611ba58877bfe6
markumichaeloh@gmail.com <markumichaeloh@gmail.com>
2024-06-29 12:05:53 +0900

    I study application supplimented

--- Slide20 - Application 2 - I study.swift
+++ Slide20 - Application 2 - I study.swift
@@ -17,6 +17,8 @@ struct Slide20: View {
             "나는 집에서 하루 6시간 동안 공부한다.",
             "I study on weekends.",
             "나는 주말에 공부합니다.",
+            "I study six hours a day at home mainly with YouTube videos.",
+            "나는 집에서 유튜브 영상을 주로 보면서 하루에 6시간 동안 공부합니다.",
         ])
     }
 }
