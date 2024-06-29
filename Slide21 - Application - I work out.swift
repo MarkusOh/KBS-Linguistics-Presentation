1a013e108efeff79ea388daf484dae54197924d5
markumichaeloh@gmail.com <markumichaeloh@gmail.com>
2024-06-29 12:08:38 +0900

    I work out supplemented

--- Slide21 - Application - I work out.swift
+++ Slide21 - Application - I work out.swift
@@ -15,6 +15,10 @@ struct Slide21: View {
             "저는 운동합니다.",
             "I work out by myself / I exercise by myself.",
             "저는 혼자서 운동합니다.",
+            "I work out on weekends.",
+            "저는 주말에 운동합니다.",
+            "I work out on weekends by myself at home.",
+            "저는 혼자 집에서 주말에 운동합니다."
         ])
     }
 }
